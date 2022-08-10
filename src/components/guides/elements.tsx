7f924861e3057db88d80e8cfbc72a93bceb43c3b
Albert Citko <albert.citko@gmail.com>
2022-08-10 10:39:17 +0200

    nice tip selection

---
+++ src/components/guides/elements.tsx
@@ -0,0 +1,121 @@
+import {
+  component$,
+  createContext,
+  Slot,
+  useContext,
+  useContextProvider,
+  useStore,
+} from "@builder.io/qwik";
+
+export const Section = component$(() => {
+  return (
+    <div class="rounded-lg bg-neutral-800 px-4 py-2 my-4">
+      <Slot />
+    </div>
+  );
+});
+
+type ClassChoice = "ham" | "pMech" | "pVirt" | "qfb" | "qscrap";
+
+export const classChoiceNew: { name: ClassChoice; roles: RoleChoice[] }[] = [
+  { name: "ham", roles: ["heal"] },
+  { name: "pMech", roles: ["dps"] },
+  { name: "pVirt", roles: ["portal", "dps"] },
+  { name: "qfb", roles: ["quick"] },
+  { name: "qscrap", roles: ["quick"] },
+];
+
+export const classChoice: ClassChoice[] = [
+  "ham",
+  "pMech",
+  "pVirt",
+  "qfb",
+  "qscrap",
+];
+type RoleChoice = "heal" | "portal" | "quick" | "dps";
+export const roleChoice: RoleChoice[] = ["heal", "portal", "quick", "dps"];
+interface TipSelection {
+  _class: ClassChoice;
+  role: RoleChoice;
+}
+
+export const TipSelectionContext = createContext<TipSelection>("TipSelection");
+
+export function useTipSelectionContextProvider() {
+  useContextProvider(
+    TipSelectionContext,
+    useStore<TipSelection>({
+      _class: "ham",
+      role: "heal",
+    })
+  );
+}
+
+export const TipBox = component$(() => {
+  return (
+    <div class="rounded-lg my-4">
+      <SelectTip />
+      <Slot />
+    </div>
+  );
+});
+
+export const SelectTip = component$(() => {
+  const selected = useContext(TipSelectionContext);
+
+  return (
+    <div class="bg-slate-800">
+      <div>
+        {classChoiceNew.map((c) => {
+          return (
+            <button
+              class="btn btn-primary btn-sm"
+              disabled={selected._class === c.name}
+              onClick$={() => {
+                selected._class = c.name;
+                selected.role = c.roles[0];
+              }}
+            >
+              {c.name}
+            </button>
+          );
+        })}
+      </div>
+      <div>
+        {classChoiceNew
+          .filter((a) => a.name === selected._class)[0]
+          ?.roles.map((r) => {
+            return (
+              <button
+                class="btn btn-secondary btn-sm"
+                disabled={selected.role === r}
+                onClick$={() => (selected.role = r)}
+              >
+                {r}
+              </button>
+            );
+          })}
+      </div>
+    </div>
+  );
+});
+
+export const SpecificTips = component$(
+  ({ _class, role }: Partial<TipSelection>) => {
+    const selected = useContext(TipSelectionContext);
+    const cMatch = _class === undefined || selected._class === _class;
+    const rMatch = role === undefined || selected.role === role;
+    if (cMatch && rMatch) {
+      return (
+        <div class="bg-neutral-700 p-4">
+          <p class="font-bold">
+            tips for {_class} {role}
+          </p>
+          <Slot />
+        </div>
+      );
+    } else {
+      return <></>;
+    }
+  }
+);
---
+++ src/components/guides/main.tsx
@@ -0,0 +1,38 @@
+import { component$, Host } from "@builder.io/qwik";
+import {
+  Section,
+  SpecificTips,
+  TipBox,
+  useTipSelectionContextProvider,
+} from "./elements";
+
+export const MainGuide = component$(() => {
+  useTipSelectionContextProvider();
+
+  return (
+    <Host>
+      Welcome to this simple guide for HT CM <br />
+      It's short and fun and I hope you like it
+      <Section>
+        <h1 class="text-2xl font-bold">Jormag</h1>
+        Hello!
+      </Section>
+      <Section>
+        <h1 class="text-2xl font-bold">Primordus</h1>
+        Hello!
+        <TipBox>
+          <SpecificTips _class="ham" role="heal">
+            tips for ham
+          </SpecificTips>
+          <SpecificTips _class="pVirt" role="portal">
+            tips for pVirt
+          </SpecificTips>
+          <SpecificTips _class="pVirt" role="dps">
+            tips for pVirt dps
+          </SpecificTips>
+          <SpecificTips role="dps">tips for dps</SpecificTips>
+        </TipBox>
+      </Section>
+    </Host>
+  );
+});
